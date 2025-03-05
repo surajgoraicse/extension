95f4e846d49b29934d149b84776235618563ad70
surajgoraicse <surajgoraicse@gmail.com>
2025-03-06 01:19:42 +0530

    top bar bookmark fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import data from "@/data/data";
 
 function Navbar() {
 	return (
-		<div className="sticky my-3  top-4 z-5 border border-gray-700/50 mx-8  lg:mx-auto max-w-4xl rounded-xl backdrop-blur-md  bg-gray-950/50 ">
+		<div className="sticky my-3  top-4 z-5 border border-gray-700/50  mx-8  lg:mx-auto max-w-4xl rounded-xl backdrop-blur-md   bg-gray-950/50 ">
 			<div className="flex items-center justify-between px-6 py-3  list-none">
 				<div className="font-bold text-lg ">Vision AI</div>
 				<div className="flex items-center justify-end flex-1 ">
